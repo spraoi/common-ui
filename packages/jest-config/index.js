3a55423ddda29394bd53e41371cbce0115fa827f
Cade Scroggins <git@cadejs.com>
2018-11-24 14:42:54 -0800

    enzyme.setup.js -> jest.setup.js

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   bail: true,
-  globalSetup: path.resolve(__dirname, 'enzyme.setup.js'),
+  globalSetup: path.resolve(__dirname, 'jest.setup.js'),
   moduleNameMapper: { '\\.css$': path.resolve(__dirname, 'jest.mock.js') },
   rootDir: process.cwd(),
   transform: { '\\.jsx?$': path.resolve(__dirname, 'jest.transform.js') },
