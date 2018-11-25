10664079d928805e678a34df9122dc8009cc0a8a
Cade Scroggins <git@cadejs.com>
2018-11-25 12:18:02 -0800

    destructure path require

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -1,18 +1,18 @@
-const path = require('path');
+const { resolve } = require('path');
 
 module.exports = {
   bail: true,
   globals: { __PATH_PREFIX__: '' },
-  globalSetup: path.resolve(__dirname, 'jest.setup.js'),
+  globalSetup: resolve(__dirname, 'jest.setup.js'),
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'jest.mock.js',
   },
   rootDir: process.cwd(),
-  setupFiles: [path.resolve(__dirname, 'jest.shim.js')],
+  setupFiles: [resolve(__dirname, 'jest.shim.js')],
   testPathIgnorePatterns: ['.cache/', '.idea/', 'node_modules/', 'public/'],
-  transform: { '^.+\\.jsx?$': path.resolve(__dirname, 'jest.transform.js') },
+  transform: { '^.+\\.jsx?$': resolve(__dirname, 'jest.transform.js') },
   transformIgnorePatterns: ['node_modules/(?!(gatsby|spraoi)/)'],
   verbose: true,
 };
