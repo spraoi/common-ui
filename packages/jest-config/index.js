f402dcca93a5282f36267f491e679032c1461cbf
Cade Scroggins <git@cadejs.com>
2018-11-25 11:10:47 -0800

    update jest config for gatsby

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 module.exports = {
   bail: true,
+  globals: { __PATH_PREFIX__: '' },
   globalSetup: path.resolve(__dirname, 'jest.setup.js'),
-  moduleNameMapper: { '\\.css$': path.resolve(__dirname, 'jest.mock.js') },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'jest.mock.js',
+  },
   rootDir: process.cwd(),
-  transform: { '\\.jsx?$': path.resolve(__dirname, 'jest.transform.js') },
+  setupFiles: [path.resolve(__dirname, 'jest.shim.js')],
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  transform: { '^.+\\.jsx?$': path.resolve(__dirname, 'jest.transform.js') },
+  transformIgnorePatterns: ['node_modules/(?!(gatsby|spraoi)/)'],
   verbose: true,
 };
