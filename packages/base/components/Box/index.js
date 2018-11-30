564c29dd6acc46b0c8b9f37b6706ad3482370a98
Cade Scroggins <git@cadejs.com>
2018-11-30 01:03:26 -0800

    messing with boxes

--- packages/base/components/Box/index.js
+++ packages/base/components/Box/index.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Box = styled.span`
+const themeOrValue = (props, theme, or) => {
+  const value = props.find(p => p) || or;
+  return theme[value] || value;
+};
+
+const Box = styled.div`
   display: ${p => p.display};
   justify-content: ${p => p.justifyContent};
   align-items: ${p => p.alignItems};
-  margin-top: ${p => p.theme.space[p.mt || p.my || p.m]};
-  margin-right: ${p => p.theme.space[p.mr || p.mx || p.m]};
-  margin-bottom: ${p => p.theme.space[p.mb || p.my || p.m]};
-  margin-left: ${p => p.theme.space[p.ml || p.mx || p.m]};
+  max-width: ${p => p.theme.maxWidths[p.maxWidth] || p.maxWidth};
+  margin-top: ${p => themeOrValue([p.mt, p.my, p.m], p.theme.space, 0)};
+  margin-right: ${p => themeOrValue([p.mr || p.mx || p.m], p.theme.space, 0)};
+  margin-bottom: ${p => themeOrValue([p.mb || p.my || p.m], p.theme.space, 0)};
+  margin-left: ${p => themeOrValue([p.ml || p.mx || p.m], p.theme.space, 0)};
   padding-top: ${p => p.theme.space[p.pt || p.py || p.p]};
   padding-right: ${p => p.theme.space[p.pr || p.px || p.p]};
   padding-bottom: ${p => p.theme.space[p.pb || p.py || p.p]};
@@ -23,38 +29,40 @@ Box.propTypes = {
   color: PropTypes.string,
   display: PropTypes.string,
   justifyContent: PropTypes.string,
-  mb: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  ml: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  mr: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  mt: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  mx: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  my: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  pb: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  pl: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  pr: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  pt: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  px: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  py: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  maxWidth: PropTypes.string,
+  mb: PropTypes.string,
+  ml: PropTypes.string,
+  mr: PropTypes.string,
+  mt: PropTypes.string,
+  mx: PropTypes.string,
+  my: PropTypes.string,
+  pb: PropTypes.string,
+  pl: PropTypes.string,
+  pr: PropTypes.string,
+  pt: PropTypes.string,
+  px: PropTypes.string,
+  py: PropTypes.string,
 };
 
 Box.defaultProps = {
-  alignItems: 'normal',
-  backgroundColor: 'inherit',
-  color: 'inherit',
-  display: 'inherit',
-  justifyContent: 'initial',
-  mb: 0,
-  ml: 0,
-  mr: 0,
-  mt: 0,
-  mx: 0,
-  my: 0,
-  pb: 0,
-  pl: 0,
-  pr: 0,
-  pt: 0,
-  px: 0,
-  py: 0,
+  alignItems: null,
+  backgroundColor: null,
+  color: null,
+  display: null,
+  justifyContent: null,
+  maxWidth: null,
+  mb: null,
+  ml: null,
+  mr: null,
+  mt: null,
+  mx: null,
+  my: null,
+  pb: null,
+  pl: null,
+  pr: null,
+  pt: null,
+  px: null,
+  py: null,
 };
 
 export default Box;
