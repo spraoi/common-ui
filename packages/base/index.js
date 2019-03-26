36a01eec79bb6769be3a59e17089d4334ade8207
Cade Scroggins <git@cadejs.com>
2019-03-26 14:31:34 -0700

    properly export Dropdown

--- packages/base/index.js
+++ packages/base/index.js
@@ -1,7 +1,7 @@
 import App from './components/App';
 import Box from './components/Box';
 import Button from './components/Button';
-import { Input, TextArea } from './components/Input';
+import { Dropdown, Input, TextArea } from './components/Input';
 import Redirect from './components/Redirect';
 
-export { App, Box, Button, Redirect, Input, TextArea };
+export { App, Box, Button, Dropdown, Redirect, Input, TextArea };
