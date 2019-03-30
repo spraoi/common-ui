9a9a7f96b17905c74e6304a542d78a851bdf960e
Cade Scroggins <git@cadejs.com>
2019-03-29 19:40:16 -0700

    add shortenNumber

--- packages/helpers/__tests__/index.test.js
+++ packages/helpers/__tests__/index.test.js
@@ -2,6 +2,7 @@ import {
   fixSCProps,
   objectMapDeep,
   objectMapKeysDeep,
+  shortenNumber,
   snakeCaseToCamelCase,
 } from '..';
 
@@ -93,6 +94,26 @@ describe('objectMapKeysDeep', () => {
       },
     });
   });
+});
+
+describe('shortenNumber', () => {
+  [
+    [-1, '<1'],
+    [0.3, '<1'],
+    [10, '10'],
+    [1000, '1K'],
+    [1000.001, '1K'],
+    [1500, '1.5K'],
+    [10003, '10K'],
+    [18000, '18K'],
+    [1000000, '1M'],
+    [1040000000, '1.04B'],
+    [1000000000000, '1T'],
+    [1000000000000000, '1Q'],
+  ].forEach(([input, output]) =>
+    it(`should shorten ${input} to ${output}`, () =>
+      expect(shortenNumber(input)).toEqual(output))
+  );
 });
 
 describe('snakeCaseToCamelCase', () => {
--- packages/helpers/index.js
+++ packages/helpers/index.js
@@ -61,6 +61,40 @@ export const objectMapKeysDeep = (obj, callback) => {
 };
 
 /**
+ * Shorten a number.
+ * @param number
+ * @returns {string}
+ */
+export const shortenNumber = number => {
+  if (number < 1) return '<1';
+  const base = Math.floor(number);
+
+  const shorten = (size, suffix) => {
+    const parts = size.toString().split('.');
+    const fraction = parts[1] ? parts[1].substring(0, 2) : null;
+    if (!fraction || /^0+$/.test(fraction)) return `${parts[0]}${suffix}`;
+    return `${parts[0]}.${fraction}${suffix}`;
+  };
+
+  let size = base / 1e15;
+  if (size >= 1) return shorten(size, 'Q');
+
+  size = base / 1e12;
+  if (size >= 1) return shorten(size, 'T');
+
+  size = base / 1e9;
+  if (size >= 1) return shorten(size, 'B');
+
+  size = base / 1e6;
+  if (size >= 1) return shorten(size, 'M');
+
+  size = base / 1e3;
+  if (size >= 1) return shorten(size, 'K');
+
+  return base.toString();
+};
+
+/**
  * Convert a snake_case_string to a camelCaseString.
  * @param {*} string
  * @returns {*}
