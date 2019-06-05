4c61240dd1152e0d660869c4ea188b1d9273f1e8
Cade Scroggins <git@cadejs.com>
2019-06-05 12:57:54 -0700

    smarter dropdown

--- packages/base/components/Input/Dropdown.js
+++ packages/base/components/Input/Dropdown.js
@@ -1,100 +1,111 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import AsyncSelect from 'react-select/lib/Async';
 import { ThemeConsumer } from 'styled-components';
 import InputWrapper from './InputWrapper';
 
-const Dropdown = ({ input, ...rest }) => (
-  <InputWrapper input={input} {...rest}>
-    {({
-      backspaceRemoves = false,
-      loadOptions,
-      placeholder = '',
-      ...inputRest
-    }) => (
-      <ThemeConsumer>
-        {theme => {
-          const height = `calc(${theme.space.xs} * 2 + ${
-            theme.lineHeights.md
-          })`;
+const Dropdown = ({ input, ...rest }) => {
+  const [asyncOptions, setAsyncOptions] = useState([]);
 
-          const getBorderColor = ({ isFocused }) => {
-            if (isFocused) return theme.colors.inputPrimaryBorderFocus;
-            if (inputRest.error) return theme.colors.error;
-            return theme.colors.inputPrimaryBorder;
-          };
+  return (
+    <InputWrapper input={input} {...rest}>
+      {({
+        backspaceRemoves = false,
+        loadOptions,
+        placeholder = '',
+        ...inputRest
+      }) => (
+        <ThemeConsumer>
+          {theme => {
+            const height = `calc(${theme.space.xs} * 2 + ${
+              theme.lineHeights.md
+            })`;
 
-          const styles = {
-            control: (base, { isFocused }) => ({
-              ...base,
-              '&:hover': {
+            const getBorderColor = ({ isFocused }) => {
+              if (isFocused) return theme.colors.inputPrimaryBorderFocus;
+              if (inputRest.error) return theme.colors.error;
+              return theme.colors.inputPrimaryBorder;
+            };
+
+            const styles = {
+              control: (base, { isFocused }) => ({
+                ...base,
+                '&:hover': {
+                  border: `solid 1px ${getBorderColor({ isFocused })}`,
+                },
+                backgroundColor: theme.colors.inputPrimaryBg,
                 border: `solid 1px ${getBorderColor({ isFocused })}`,
-              },
-              backgroundColor: theme.colors.inputPrimaryBg,
-              border: `solid 1px ${getBorderColor({ isFocused })}`,
-              borderRadius: theme.radii.md,
-              boxShadow: 0,
-              minHeight: height,
-            }),
-            option: base => ({
-              ...base,
-              // TODO: customize
-            }),
-            placeholder: base => ({
-              ...base,
-              color: theme.colors.inputPrimaryPlaceholderText,
-            }),
-            valueContainer: base => ({
-              ...base,
-              border: 0,
-              borderRadius: theme.radii.md,
-            }),
-          };
+                borderRadius: theme.radii.md,
+                boxShadow: 0,
+                minHeight: height,
+              }),
+              option: base => ({
+                ...base,
+                // TODO: customize
+              }),
+              placeholder: base => ({
+                ...base,
+                color: theme.colors.inputPrimaryPlaceholderText,
+              }),
+              valueContainer: base => ({
+                ...base,
+                border: 0,
+                borderRadius: theme.radii.md,
+              }),
+            };
 
-          const onChange = (value, meta) =>
-            input.onChange(
-              Array.isArray(value) ? value.map(o => o.value) : value.value,
-              meta
+            const onChange = (value, meta) => {
+              const parsedValue = Array.isArray(value)
+                ? value.map(o => o.value)
+                : value.value;
+
+              if (inputRest.onChange) inputRest.onChange(parsedValue, meta);
+              input.onChange(parsedValue, meta);
+            };
+
+            const optionByValue = value =>
+              (inputRest.options || asyncOptions).find(o => o.value === value);
+
+            const value = Array.isArray(input.value)
+              ? input.value.map(optionByValue)
+              : optionByValue(input.value);
+
+            return loadOptions ? (
+              <AsyncSelect
+                {...inputRest}
+                {...input}
+                backspaceRemoves={backspaceRemoves}
+                cacheOptions
+                defaultOptions
+                loadOptions={async query => {
+                  const options = await loadOptions(query);
+                  setAsyncOptions(options);
+                  return options;
+                }}
+                onChange={onChange}
+                placeholder={placeholder}
+                styles={styles}
+                value={value}
+              />
+            ) : (
+              <Select
+                {...inputRest}
+                {...input}
+                backspaceRemoves={backspaceRemoves}
+                defaultValue={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                styles={styles}
+                value={value}
+              />
             );
-
-          const optionByValue = value =>
-            inputRest.options.find(o => o.value === value);
-
-          const value = Array.isArray(input.value)
-            ? input.value.map(optionByValue)
-            : optionByValue(input.value);
-
-          return loadOptions ? (
-            <AsyncSelect
-              {...input}
-              backspaceRemoves={backspaceRemoves}
-              cacheOptions
-              defaultOptions
-              loadOptions={loadOptions}
-              onChange={onChange}
-              placeholder={placeholder}
-              styles={styles}
-              value={value}
-              {...inputRest}
-            />
-          ) : (
-            <Select
-              {...input}
-              backspaceRemoves={backspaceRemoves}
-              defaultValue={value}
-              onChange={onChange}
-              placeholder={placeholder}
-              styles={styles}
-              value={value}
-              {...inputRest}
-            />
-          );
-        }}
-      </ThemeConsumer>
-    )}
-  </InputWrapper>
-);
+          }}
+        </ThemeConsumer>
+      )}
+    </InputWrapper>
+  );
+};
 
 Dropdown.propTypes = {
   input: PropTypes.shape({ name: PropTypes.string.isRequired }).isRequired,
