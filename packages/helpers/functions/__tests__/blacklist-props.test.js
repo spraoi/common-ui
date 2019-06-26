697641cef9a7f45891734ffd5ef6bd73642a6b6c
Cade Scroggins <git@cadejs.com>
2019-06-26 13:45:19 -0700

    100% coverage

--- packages/helpers/functions/__tests__/blacklist-props.test.js
+++ packages/helpers/functions/__tests__/blacklist-props.test.js
@@ -3,6 +3,18 @@ import blacklistProps from '../blacklist-props';
 describe('blacklistProps helper', () => {
   it('should work', () => {
     expect(
+      blacklistProps({ blacklist: ['hide'] })({
+        hide: 'foo',
+        show: 'bar',
+      })
+    ).toMatchObject({
+      props: { show: 'bar' },
+      type: 'div',
+    });
+  });
+
+  it('should work as a different element', () => {
+    expect(
       blacklistProps({ as: 'span', blacklist: ['hide'] })({
         hide: 'foo',
         show: 'bar',
@@ -10,6 +22,13 @@ describe('blacklistProps helper', () => {
     ).toMatchObject({
       props: { show: 'bar' },
       type: 'span',
+    });
+  });
+
+  it('should work when no blacklist is set', () => {
+    expect(blacklistProps()({ hide: 'foo', show: 'bar' })).toMatchObject({
+      props: { hide: 'foo', show: 'bar' },
+      type: 'div',
     });
   });
 });
