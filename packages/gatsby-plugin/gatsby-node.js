081794101d6743dda6b2554b1503271514044539
Cade Scroggins <git@cadejs.com>
2019-06-25 19:14:05 -0700

    add icon generator

--- packages/gatsby-plugin/gatsby-node.js
+++ packages/gatsby-plugin/gatsby-node.js
@@ -1,6 +1,7 @@
-const { readFileSync, writeFileSync } = require('fs');
-const { safeLoad } = require('js-yaml');
+const fs = require('fs');
 const merge = require('deepmerge');
+const prettier = require('prettier');
+const { safeLoad } = require('js-yaml');
 
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   const config = getConfig();
@@ -22,6 +23,11 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
 };
 
 module.exports.onPreInit = () => {
+  createConfigJson();
+  createIconComponent();
+};
+
+function createConfigJson() {
   const [variation, stage] = process.env.config.split('.');
 
   const config = [
@@ -31,11 +37,73 @@ module.exports.onPreInit = () => {
     `configs/${variation}.${stage}.yml`,
   ].reduce((acc, file) => {
     try {
-      return merge(acc, safeLoad(readFileSync(file, 'utf8')));
+      return merge(acc, safeLoad(fs.readFileSync(file, 'utf8')));
     } catch (e) {
       return acc;
     }
   }, {});
 
-  writeFileSync('src/config.json', JSON.stringify(config, null, 2));
-};
+  fs.writeFileSync('src/config.json', JSON.stringify(config, null, 2));
+}
+
+function createIconComponent() {
+  const iconDir = 'src/images/icons';
+  const iconComponentDir = 'src/components/Icon';
+  const iconComponent = `${iconComponentDir}/index.js`;
+
+  const files = fs
+    .readdirSync(iconDir)
+    .map(file => ({
+      componentName: file.split('.')[0].replace(/[^a-z]/, ''),
+      file,
+      name: file.split('.')[0],
+      type: file.split('.').pop(),
+    }))
+    .filter(file => file.type === 'svg');
+
+  if (!fs.existsSync(iconComponentDir)) fs.mkdirSync(iconComponentDir);
+
+  prettier.resolveConfig(iconComponent).then(options => {
+    const allOpts = { ...options, parser: 'babel' };
+
+    const formatted = prettier.format(
+      `
+        // NOTE: this file is generated automatically, any changes will be overridden.
+
+        import PropTypes from 'prop-types';
+        import React from 'react';
+        import { Box } from '@spraoi/base';
+        ${files.reduce(
+          (acc, file) =>
+            `${acc}import ${file.componentName} from '../../images/icons/${file.file}';`,
+          ''
+        )}
+
+        const svgMap = {
+          ${files.map(file => `'${file.name}': ${file.componentName}`)}
+        };
+
+        const Icon = ({ svg, ...rest }) => {
+          const SvgComponent = svgMap[svg];
+
+          return (
+            <Box {...rest}>
+              <SvgComponent />
+            </Box>
+          );
+        };
+
+        Icon.propTypes = {
+          svg: PropTypes.oneOf([
+            ${files.map(file => `'${file.name}'`)}
+          ]).isRequired,
+        };
+
+        export default Icon;
+      `,
+      allOpts
+    );
+
+    fs.writeFileSync(iconComponent, formatted);
+  });
+}
