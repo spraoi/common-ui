58c7b318d740f6154e3a7df61d8c3dbeabca1f46
Varun Patel <Varun@Varuns-MacBook-Air.local>
2019-07-25 15:23:26 +0530

    manage the async stuff outside of the dropdown

--- packages/base/components/Input/Dropdown.js
+++ packages/base/components/Input/Dropdown.js
@@ -7,11 +7,26 @@ import InputWrapper from './InputWrapper';
 
 const Dropdown = ({ input, ...rest }) => {
   const [asyncOptions, setAsyncOptions] = useState([]);
-
+  /* This function will remove duplicate objects from array */
+  const uniqueArray = array => {
+    const result = [];
+    const map = new Map();
+    array.forEach(item => {
+      if (!map.has(item.value)) {
+        map.set(item.value, true);
+        result.push({
+          label: item.label,
+          value: item.value,
+        });
+      }
+    });
+    return result;
+  };
   return (
     <InputWrapper input={input} {...rest}>
       {({
         backspaceRemoves = false,
+        cacheOptions = false,
         loadOptions,
         placeholder = '',
         ...inputRest
@@ -27,6 +42,10 @@ const Dropdown = ({ input, ...rest }) => {
             };
 
             const styles = {
+              container: base => ({
+                ...base,
+                flex: 1,
+              }),
               control: (base, { isFocused }) => ({
                 ...base,
                 '&:hover': {
@@ -64,8 +83,13 @@ const Dropdown = ({ input, ...rest }) => {
               input.onChange(parsedValue, meta);
             };
 
-            const optionByValue = value =>
-              (inputRest.options || asyncOptions).find(o => o.value === value);
+            const optionByValue = value => {
+              // After search add new options in state
+              const newAsyncOptions = rest.asyncPrimaryOptions || asyncOptions;
+              return [...(inputRest.options || []), ...newAsyncOptions].find(
+                o => o.value === value
+              );
+            };
 
             const value = Array.isArray(input.value)
               ? input.value.map(optionByValue)
@@ -76,11 +100,18 @@ const Dropdown = ({ input, ...rest }) => {
                 {...inputRest}
                 {...input}
                 backspaceRemoves={backspaceRemoves}
-                cacheOptions
+                cacheOptions={cacheOptions}
                 defaultOptions
                 loadOptions={async query => {
                   const options = await loadOptions(query);
-                  setAsyncOptions(options);
+                  if (inputRest.asyncPrimaryOptions) {
+                    /* After search add new options in state & remove duplicate options from state */
+                    inputRest.setAsyncPrimaryOptions(prevOptions =>
+                      uniqueArray([...prevOptions, ...options])
+                    );
+                  } else {
+                    setAsyncOptions(options);
+                  }
                   return options;
                 }}
                 onChange={onChange}
