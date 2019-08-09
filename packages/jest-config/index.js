3d5944c824728cdb4d83f27b378a2eaacd7a19e5
Cade Scroggins <git@cadejs.com>
2019-08-09 15:15:28 -0700

    update jest config

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 
 module.exports = {
   bail: true,
+  collectCoverage: true,
   globals: { __PATH_PREFIX__: '' },
   globalSetup: resolve(__dirname, 'jest.setup.js'),
   moduleNameMapper: {
@@ -9,6 +10,7 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'jest.mock.js',
   },
+  reporters: ['default', ['jest-junit', { outputDirectory: 'reports' }]],
   rootDir: process.cwd(),
   setupFiles: [resolve(__dirname, 'jest.shim.js')],
   testPathIgnorePatterns: ['.cache/', 'node_modules/', 'public/'],
