6e4e4bb47a80b5c3dc9e60925e90400522832b3f
Cade Scroggins <git@cadejs.com>
2019-08-24 21:04:07 -0700

    legacy test pass exit code along

--- packages/legacy-operations/bin/test.js
+++ packages/legacy-operations/bin/test.js
@@ -3,4 +3,5 @@
 const path = require('path');
 const shell = require('shelljs');
 const configFile = path.resolve(__dirname, '../jest.config.js');
-shell.exec(`jest --config ${configFile}`);
+
+shell.exit(shell.exec(`jest --config ${configFile}`).code);
