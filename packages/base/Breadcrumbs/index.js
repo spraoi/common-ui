0787745df5f147af877fe1681b6a357731eb8530
Varun Patel <Varun@Varuns-MacBook-Air.local>
2020-03-23 23:32:16 +0530

    onclick updated

--- packages/base/Breadcrumbs/index.js
+++ packages/base/Breadcrumbs/index.js
@@ -9,7 +9,7 @@ const Breadcrumbs = ({ crumbs, crumbSx, separator, ...rest }) => (
       <Button
         key={i}
         link={crumb.link}
-        onClick={crumb.onClick ? () => crumb.onClick() : crumb.onClick}
+        onClick={crumb.onClick}
         separator={separator}
         simple
         sx={{
