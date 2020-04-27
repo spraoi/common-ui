12089cc87606af98ebd5e854b191ec3044ea75c1
Bhargav K <bhargav.kahar@everestek.com>
2020-04-27 16:52:11 +0530

    Full pagination feature added.

--- packages/base/Paginator/index.js
+++ packages/base/Paginator/index.js
@@ -1,5 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
+
 import Box from '../Box';
 import Button from '../Button';
 
@@ -15,8 +16,10 @@ const paginationButtonSx = {
 };
 
 const Paginator = ({
+  fullPagintation,
   leftButtonContent,
   offset,
+  onPageClick,
   onNextClick,
   onPreviousClick,
   pageSize,
@@ -24,45 +27,88 @@ const Paginator = ({
   rightButtonContent,
   total,
   ...rest
-}) => (
-  <Box
-    alignItems="center"
-    display="flex"
-    justifyContent="flex-end"
-    mt={5}
-    {...rest}
-  >
-    {pageTotal > 0 && (
-      <Box mr={3}>
-        {offset + 1}&ndash;{offset + pageTotal}
-        {total ? ` of ${total}` : ''}
-      </Box>
-    )}
-    <Button
-      disabled={offset === 0}
-      onClick={onPreviousClick}
-      sx={paginationButtonSx}
+}) => {
+  const [currPage, setCurrPage] = useState(1);
+  const pagiBtns = [];
+
+  useEffect(() => {
+    setCurrPage(parseInt(offset / pageSize, 10) + 1);
+  }, [offset]);
+
+  if (fullPagintation) {
+    const reminder = parseInt(total / pageSize, 10);
+    const noOfPages =
+      parseInt(total % pageSize, 10) === 0 ? reminder : reminder + 1;
+
+    for (let i = 1; i <= noOfPages; i += 1) {
+      pagiBtns.push(
+        <Button
+          key={i}
+          onClick={() => {
+            setCurrPage(i);
+            onPageClick(i - 1);
+          }}
+          sx={{
+            ...paginationButtonSx,
+            bg: currPage === i ? 'grays.5' : 'grays.4',
+          }}
+        >
+          {i}
+        </Button>
+      );
+    }
+  }
+
+  return (
+    <Box
+      alignItems="center"
+      display="flex"
+      justifyContent="flex-end"
+      mt={5}
+      {...rest}
     >
-      {leftButtonContent}
-    </Button>
-    <Button
-      disabled={
-        total
-          ? offset + pageSize >= total
-          : pageTotal === 0 || pageTotal % pageSize !== 0
-      }
-      onClick={onNextClick}
-      sx={paginationButtonSx}
-    >
-      {rightButtonContent}
-    </Button>
-  </Box>
-);
+      {pageTotal > 0 && (
+        <Box mr={3}>
+          {offset + 1}&ndash;{offset + pageTotal}
+          {total ? ` of ${total}` : ''}
+        </Box>
+      )}
+
+      <Button
+        disabled={offset === 0}
+        onClick={() => {
+          setCurrPage(currPage - 1);
+          onPreviousClick();
+        }}
+        sx={paginationButtonSx}
+      >
+        {leftButtonContent}
+      </Button>
+      {fullPagintation && pageTotal > 0 && pagiBtns}
+      <Button
+        disabled={
+          total
+            ? offset + pageSize >= total
+            : pageTotal === 0 || pageTotal % pageSize !== 0
+        }
+        onClick={() => {
+          setCurrPage(currPage + 1);
+          onNextClick();
+        }}
+        sx={paginationButtonSx}
+      >
+        {rightButtonContent}
+      </Button>
+    </Box>
+  );
+};
 
 Paginator.propTypes = {
+  fullPagintation: PropTypes.bool,
   leftButtonContent: PropTypes.node.isRequired,
   offset: PropTypes.number,
   onNextClick: PropTypes.func.isRequired,
+  onPageClick: PropTypes.func,
   onPreviousClick: PropTypes.func.isRequired,
   pageSize: PropTypes.number,
   pageTotal: PropTypes.number.isRequired,
@@ -71,7 +117,9 @@ Paginator.propTypes = {
 };
 
 Paginator.defaultProps = {
+  fullPagintation: false,
   offset: 0,
+  onPageClick: () => {},
   pageSize: 10,
   total: null,
 };
