078096949ea84609f9497b1bf6d49bee7b91c71c
Varun Patel <Varun@Varuns-MacBook-Air.local>
2020-04-28 12:44:42 +0530

    Refactor: horizontal bar chart update axises https://app.asana.com/0/793270570460406/1171418008766729

--- packages/charts/HorizontalBarChart/index.js
+++ packages/charts/HorizontalBarChart/index.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
 import { TooltipBox } from '@spraoi/base/Tooltip';
 import { AxisBottom, AxisLeft } from '@vx/axis';
-import { Bar } from '@vx/shape';
+import { Bar, Line } from '@vx/shape';
 import { GridColumns } from '@vx/grid';
 import { Group } from '@vx/group';
 import { ParentSize } from '@vx/responsive';
@@ -11,7 +11,14 @@ import { scaleBand, scaleLinear } from '@vx/scale';
 import numTicksForWidth from './utilities/num-ticks-for-width';
 import { BAR_PADDING } from './utilities/constants';
 
-const HorizontalBarChart = ({ data, margin, xAccessor, yAccessor }) => {
+const HorizontalBarChart = ({
+  data,
+  margin,
+  xAccessor,
+  xAxisProps,
+  yAccessor,
+  yAxisProps,
+}) => {
   const theme = useContext(ThemeContext);
 
   return (
@@ -58,30 +65,126 @@ const HorizontalBarChart = ({ data, margin, xAccessor, yAccessor }) => {
                   />
                 ))}
                 <AxisLeft
-                  hideAxisLine
-                  hideTicks
+                  label={yAxisProps.label}
                   scale={yScale}
                   stroke={theme.colors.grays[1]}
-                  tickLabelProps={() => ({
-                    dy: '0.33em',
-                    fill: theme.colors.textSubtle,
-                    fontSize: theme.fontSizes[1],
-                    textAnchor: 'end',
-                  })}
-                />
+                >
+                  {(axis) => {
+                    const axisCenter =
+                      (axis.axisToPoint.y - axis.axisFromPoint.y) / 2;
+                    return (
+                      <g className="my-custom-bottom-axis">
+                        {!yAxisProps.hideAxisLine && (
+                          <Line
+                            from={axis.axisFromPoint}
+                            stroke={theme.colors.textSubtle}
+                            strokeWidth={yAxisProps.strokeWidth}
+                            style={{ pointerEvents: 'none' }}
+                            to={axis.axisToPoint}
+                          />
+                        )}
+                        {axis.ticks.map((tick, i) => {
+                          const tickX =
+                            tick.to.x -
+                            axis.tickLength -
+                            yAxisProps.anchorMargin;
+                          const tickY = tick.to.y;
+                          return (
+                            <Group key={`vx-tick-${tick.value}-${i}`}>
+                              {!yAxisProps.hideTicks && (
+                                <Line
+                                  from={tick.from}
+                                  stroke={theme.colors.textSubtle}
+                                  strokeWidth={yAxisProps.strokeWidth}
+                                  style={{ pointerEvents: 'none' }}
+                                  to={tick.to}
+                                />
+                              )}
+                              <text
+                                fill={theme.colors.textSubtle}
+                                fontSize={theme.fontSizes[1]}
+                                textAnchor={yAxisProps.anchorPosition}
+                                transform={`translate(${tickX}, ${tickY}) rotate(${yAxisProps.rotateAnchorDegree})`}
+                              >
+                                {tick.formattedValue}
+                              </text>
+                            </Group>
+                          );
+                        })}
+                        <text
+                          fontSize={theme.fontSizes[1]}
+                          textAnchor="middle"
+                          transform={`translate(-${
+                            margin.left - 10
+                          }, ${axisCenter}) rotate(${
+                            yAxisProps.rotateLabelDegree
+                          })`}
+                        >
+                          {axis.label}
+                        </text>
+                      </g>
+                    );
+                  }}
+                </AxisLeft>
                 <AxisBottom
+                  label={xAxisProps.label}
                   scale={xScale}
                   stroke={theme.colors.grays[1]}
-                  tickLabelProps={() => ({
-                    dy: '0.33em',
-                    fill: theme.colors.textSubtle,
-                    fontSize: theme.fontSizes[1],
-                    textAnchor: 'middle',
-                    y: 0,
-                  })}
-                  tickStroke={theme.colors.grays[1]}
                   top={yMax}
-                />
+                >
+                  {(axis) => {
+                    const axisCenter =
+                      (axis.axisToPoint.x - axis.axisFromPoint.x) / 2;
+                    return (
+                      <g className="my-custom-bottom-axis">
+                        {!xAxisProps.hideAxisLine && (
+                          <Line
+                            from={axis.axisFromPoint}
+                            stroke={theme.colors.textSubtle}
+                            strokeWidth={xAxisProps.strokeWidth}
+                            style={{ pointerEvents: 'none' }}
+                            to={axis.axisToPoint}
+                          />
+                        )}
+                        {axis.ticks.map((tick, i) => {
+                          const tickX = tick.to.x;
+                          const tickY =
+                            tick.to.y +
+                            axis.tickLength +
+                            xAxisProps.anchorMargin;
+                          return (
+                            <Group key={`vx-tick-${tick.value}-${i}`}>
+                              {!xAxisProps.hideTicks && (
+                                <Line
+                                  from={tick.from}
+                                  stroke={theme.colors.textSubtle}
+                                  strokeWidth={xAxisProps.strokeWidth}
+                                  style={{ pointerEvents: 'none' }}
+                                  to={tick.to}
+                                />
+                              )}
+                              <text
+                                fill={theme.colors.textSubtle}
+                                fontSize={theme.fontSizes[1]}
+                                textAnchor={xAxisProps.anchorPosition}
+                                transform={`translate(${tickX}, ${tickY}) rotate(${xAxisProps.rotateAnchorDegree})`}
+                              >
+                                {tick.formattedValue}
+                              </text>
+                            </Group>
+                          );
+                        })}
+                        <text
+                          fontSize={theme.fontSizes[1]}
+                          textAnchor="middle"
+                          transform={`translate(${axisCenter}, ${margin.bottom}) rotate(${xAxisProps.rotateLabelDegree})`}
+                        >
+                          {axis.label}
+                        </text>
+                      </g>
+                    );
+                  }}
+                </AxisBottom>
               </Group>
             </svg>
             <TooltipBox id="chart" />
@@ -101,7 +204,50 @@ HorizontalBarChart.propTypes = {
     top: PropTypes.number.isRequired,
   }).isRequired,
   xAccessor: PropTypes.func.isRequired,
+  xAxisProps: PropTypes.shape({
+    anchorMargin: PropTypes.number,
+    anchorPosition: PropTypes.string,
+    hideAxisLine: PropTypes.bool,
+    hideTicks: PropTypes.bool,
+    label: PropTypes.string,
+    rotateAnchorDegree: PropTypes.number,
+    rotateLabelDegree: PropTypes.number,
+    strokeWidth: PropTypes.number,
+  }),
   yAccessor: PropTypes.func.isRequired,
+  yAxisProps: PropTypes.shape({
+    anchorMargin: PropTypes.number,
+    anchorPosition: PropTypes.string,
+    hideAxisLine: PropTypes.bool,
+    hideTicks: PropTypes.bool,
+    label: PropTypes.string,
+    rotateAnchorDegree: PropTypes.number,
+    rotateLabelDegree: PropTypes.number,
+    strokeWidth: PropTypes.number,
+  }),
+};
+
+HorizontalBarChart.defaultProps = {
+  xAxisProps: {
+    anchorMargin: 5,
+    anchorPosition: 'middle',
+    hideAxisLine: false,
+    hideTicks: false,
+    label: '',
+    rotateAnchorDegree: 0,
+    rotateLabelDegree: 0,
+    strokeWidth: 0.5,
+  },
+  yAxisProps: {
+    anchorMargin: 5,
+    anchorPosition: 'middle',
+    hideAxisLine: false,
+    hideTicks: false,
+    label: '',
+    rotateAnchorDegree: 0,
+    rotateLabelDegree: 0,
+    strokeWidth: 0.5,
+  },
 };
 
 export default HorizontalBarChart;
