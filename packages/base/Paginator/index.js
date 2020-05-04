0472116c82a0c5377e259b34f846e7f0ecddf812
BHARGAV KAHAR <53463085+bhargav-kahar-everestek@users.noreply.github.com>
2020-05-04 08:17:56 +0530

    Full pagination feature added. (#56)
    * Full pagination feature added.
    
    * Pagination code improvement.
    
    * Google like pagination added.
    
    * Prop name changes in Paginator.
    
    * Btn to link changes.

--- packages/base/Paginator/index.js
+++ packages/base/Paginator/index.js
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Box from '../Box';
 import Button from '../Button';
 
@@ -15,65 +16,118 @@ const paginationButtonSx = {
 };
 
 const Paginator = ({
+  enableJumpToPage,
   leftButtonContent,
   offset,
+  onPageClick,
   onNextClick,
   onPreviousClick,
   pageSize,
   pageTotal,
   rightButtonContent,
   total,
+  totalJumpToPages,
   ...rest
-}) => (
-  <Box
-    alignItems="center"
-    display="flex"
-    justifyContent="flex-end"
-    mt={5}
-    {...rest}
-  >
-    {pageTotal > 0 && (
-      <Box mr={3}>
-        {offset + 1}&ndash;{offset + pageTotal}
-        {total ? ` of ${total}` : ''}
-      </Box>
-    )}
-    <Button
-      disabled={offset === 0}
-      onClick={onPreviousClick}
-      sx={paginationButtonSx}
+}) => {
+  const currentPage = Math.floor(offset / pageSize) + 1;
+  const pagiBtns = [];
+
+  if (enableJumpToPage) {
+    const totalPages = Math.ceil(total / pageSize);
+    const MID = Math.floor(totalJumpToPages / 2);
+
+    let start = 1;
+    let end = Math.min(totalJumpToPages, totalPages);
+
+    if (currentPage > MID + 1) {
+      end = Math.min(currentPage + MID - 1, totalPages);
+      start = currentPage - MID;
+      start = Math.min(start, end - totalJumpToPages + 1);
+    }
+
+    for (let i = start; i <= end; i += 1) {
+      pagiBtns.push(
+        <Button
+          key={i}
+          onClick={() => {
+            onPageClick(i - 1);
+          }}
+          px={2}
+          simple
+          sx={{
+            '&:hover': { color: 'grays.5', textDecoration: 'underline' },
+            color: currentPage === i ? 'grays.5' : 'grays.4',
+          }}
+        >
+          {i}
+        </Button>
+      );
+    }
+  }
+
+  return (
+    <Box
+      alignItems="center"
+      display="flex"
+      justifyContent="flex-end"
+      mt={5}
+      {...rest}
     >
-      {leftButtonContent}
-    </Button>
-    <Button
-      disabled={
-        total
-          ? offset + pageSize >= total
-          : pageTotal === 0 || pageTotal % pageSize !== 0
-      }
-      onClick={onNextClick}
-      sx={paginationButtonSx}
-    >
-      {rightButtonContent}
-    </Button>
-  </Box>
-);
+      {pageTotal > 0 && (
+        <Box mr={3}>
+          {offset + 1}&ndash;{offset + pageTotal}
+          {total ? ` of ${total}` : ''}
+        </Box>
+      )}
+
+      <Button
+        disabled={offset === 0}
+        onClick={() => {
+          onPreviousClick();
+        }}
+        sx={paginationButtonSx}
+      >
+        {leftButtonContent}
+      </Button>
+      {enableJumpToPage && pageTotal > 0 && pagiBtns}
+      <Button
+        disabled={
+          total
+            ? offset + pageSize >= total
+            : pageTotal === 0 || pageTotal % pageSize !== 0
+        }
+        onClick={() => {
+          onNextClick();
+        }}
+        sx={paginationButtonSx}
+      >
+        {rightButtonContent}
+      </Button>
+    </Box>
+  );
+};
 
 Paginator.propTypes = {
+  enableJumpToPage: PropTypes.bool,
   leftButtonContent: PropTypes.node.isRequired,
   offset: PropTypes.number,
   onNextClick: PropTypes.func.isRequired,
+  onPageClick: PropTypes.func,
   onPreviousClick: PropTypes.func.isRequired,
   pageSize: PropTypes.number,
   pageTotal: PropTypes.number.isRequired,
   rightButtonContent: PropTypes.node.isRequired,
   total: PropTypes.number,
+  totalJumpToPages: PropTypes.number,
 };
 
 Paginator.defaultProps = {
+  enableJumpToPage: false,
   offset: 0,
+  onPageClick: () => {},
   pageSize: 10,
   total: null,
+  totalJumpToPages: 10,
 };
 
 export default Paginator;
