344371416b0fbff6b5210560f4aa1b86585c3760
Varun Patel <Varun@Varuns-MacBook-Air.local>
2020-06-30 15:33:57 +0530

    Feat: add slider layout in checkbox component

--- packages/base/Checkbox/index.js
+++ packages/base/Checkbox/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 import Box from '../Box';
 import InputWrapper from '../InputWrapper';
 
-const Checkbox = ({ input, label, labelSx, type, ...rest }) => (
+const Checkbox = ({
+  input,
+  label,
+  labelSx,
+  slider,
+  sliderSx,
+  type,
+  ...rest
+}) => (
   <InputWrapper
     htmlFor={input.name}
     input={input}
@@ -12,7 +20,69 @@ const Checkbox = ({ input, label, labelSx, type, ...rest }) => (
     {...rest}
   >
     {(props) => (
-      <Box as="input" id={input.name} type={type} {...props} {...input} />
+      <>
+        {slider ? (
+          <Box
+            sx={{
+              height: '1rem',
+              position: 'relative',
+              width: '2rem',
+              ...sliderSx,
+            }}
+          >
+            <Box
+              as="input"
+              id={input.name}
+              sx={{
+                '&:checked': {
+                  '+ span': {
+                    '&:before': {
+                      borderColor: 'accent',
+                      transform: 'translateX(100%)',
+                    },
+                    bg: 'accent',
+                  },
+                },
+                cursor: 'pointer',
+                height: '100%',
+                opacity: '0',
+                position: 'relative',
+                width: '100%',
+                zIndex: 1,
+              }}
+              type={type}
+              {...props}
+              {...input}
+            />
+            <Box
+              as="span"
+              sx={{
+                '&:before': {
+                  bg: 'white',
+                  borderColor: 'grays.1',
+                  borderRadius: '50%',
+                  boxShadow: '0 0 1px 1px',
+                  content: '""',
+                  height: '100%',
+                  left: '0',
+                  position: 'absolute',
+                  top: '0',
+                  width: '50%',
+                },
+                bg: 'grays.1',
+                borderRadius: '1rem',
+                bottom: 0,
+                left: 0,
+                position: 'absolute',
+                right: 0,
+                top: 0,
+              }}
+            />
+          </Box>
+        ) : (
+          <Box as="input" id={input.name} type={type} {...props} {...input} />
+        )}
+      </>
     )}
   </InputWrapper>
 );
@@ -24,11 +94,15 @@ Checkbox.propTypes = {
   }).isRequired,
   label: PropTypes.string.isRequired,
   labelSx: PropTypes.shape({}),
+  slider: PropTypes.bool,
+  sliderSx: PropTypes.shape({}),
   type: PropTypes.oneOf(['checkbox', 'radio']),
 };
 
 Checkbox.defaultProps = {
   labelSx: {},
+  slider: false,
+  sliderSx: {},
   type: 'checkbox',
 };
 
