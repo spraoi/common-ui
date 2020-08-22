3f43caa9887f8de099337cd6f6a4d28fd0b8da12
Cade Scroggins <git@cade.me>
2020-08-22 14:16:29 -0700

    Feat: fix scroll position udpates

--- packages/gatsby-plugin/gatsby-browser.js
+++ packages/gatsby-plugin/gatsby-browser.js
@@ -6,13 +6,13 @@ exports.shouldUpdateScroll = ({
   const { prevLocation } = prevLocationObj;
   const { location } = locationObj;
 
-  if (!(prevLocation && location)) return false;
+  if (!(prevLocation && location)) return;
 
   const stripPath = (path) => path.replace(/[^a-z0-9]/gi, '');
 
   // if the path changes, we let the browser do its thing
   if (stripPath(location.pathname) !== stripPath(prevLocation.pathname)) {
-    return true;
+    return [0, 0];
   }
 
   // if a query param changes, we want to maintain our previous scroll position
